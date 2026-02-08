3364237d55f01acb9ba1ef4636ffd354ff5c37ab
Habeebamoo <habeebamoo08@gmail.com>
2026-02-08 19:10:23 +0100

    fixed errors in appeal modal

--- src/components/AppealModal.tsx
+++ src/components/AppealModal.tsx
@@ -15,7 +15,7 @@ const AppealModal = ({ setModalActive }: PropsType) => {
 
   return (
     <section className="bg-black/50 z-30 fixed top-0 bottom-0 left-0 right-0 flex-center">
-      <div className="bg-white w-[90%] sm:w-[400px] p-4 border-1 border-muted">
+      <div className="bg-white w-[90%] sm:w-100 p-4 border border-muted">
         <div onClick={closeModal} className="flex-end mb-2 cursor-pointer">
           <FaX color="red" size={15} />
         </div>
@@ -23,10 +23,10 @@ const AppealModal = ({ setModalActive }: PropsType) => {
         {/* user info */}
         <div className="flex-start gap-3">
           {/* Picture */}
-          <div className="h-10 w-10 bg-muted border-1 border-accentLight rounded-full"></div>
+          <div className="h-10 w-10 bg-muted border border-accentLight rounded-full"></div>
 
           <div>
-            <p className="font-inter">{userAppeal.userFullname}</p>
+            <p className="font-inter">{userAppeal.name}</p>
             <p className="font-inter text-accentLight text-sm">{userAppeal.username}</p>
           </div>
         </div>
@@ -34,16 +34,16 @@ const AppealModal = ({ setModalActive }: PropsType) => {
         {/* appeal message */}
         <div className="mt-6">
           <p className="font-inter">Appeal Message</p>
-          <p className="font-exo text-accent mt-2 text-sm">{userAppeal.appealMessage}</p>
+          <p className="font-exo text-accent mt-2 text-sm">{userAppeal.message}</p>
         </div>
 
         {/* actions */}
         <div className="mt-6 flex-start gap-2">
-          <button className="bg-green-600 border-1 border-green-600 cursor-pointer hover:bg-transparent hover:text-green-600 py-2 px-3 text-sm font-inter text-white rounded-md">
+          <button className="bg-green-600 border border-green-600 cursor-pointer hover:bg-transparent hover:text-green-600 py-2 px-3 text-sm font-inter text-white rounded-md">
             Accept
           </button>
 
-          <button className="bg-red-600 border-1 border-red-600 cursor-pointer hover:bg-transparent hover:text-red-600 py-2 px-3 text-sm font-inter text-white rounded-md">
+          <button className="bg-red-600 border border-red-600 cursor-pointer hover:bg-transparent hover:text-red-600 py-2 px-3 text-sm font-inter text-white rounded-md">
             Reject
           </button>
         </div>
